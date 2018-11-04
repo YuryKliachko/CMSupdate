2db1b3dfd870597d729a274bfbe3cfeb65789439
Yury Kliachko <uraklechko@gmail.com>
2018-11-04 09:56:52 +0300

    New method for local path retreiving created, lookup refactored

--- script.py
+++ script.py
@@ -13,25 +13,43 @@ File bundled in app:MyeeConfig.json
 Remote location to update from: https://s3-eu-west-1.amazonaws.com/ee-dtp-static-s3-test/prod/myeeapp/$APP_VERSION_NUMBER/myee-config-ios.json"
 NOTE!!!! The DEFAULT_APP_VERSION_NUMBER should be set for each branch depending on which version the release branch is targetted at
 """
+from os import environ
+from git import Repo
 
 app_version_number = "v4.23.0"
+project_dir = environ.get("PROJECT_DIR", None)
+repo = Repo()
+current_branch = repo.active_branch.name
+print(current_branch)
 print("\"app_version_number\" should be changed for each release manually.\n"
       "It means that each release branch which is in parallel should have its own related \"app_version_number\"")
 
-cms_config_path = "../MyEE/Resources/CMSConfiguration.json"
-watch_cms_config_path = "../WatchAppExtension/WatchCMS.json"
-app_config_file_path= "../MyEE/Resources/MyeeConfig.json"
-analytics_config_file_path= "../MyEE/Resources/AnalyticsContextData.json"
-ab_test_config_file_path= "../MyEE/Resources/abtestconfig.json"
+def get_absolute_local_path(config_name):
+    relative_local_path = {"cms_config": "/MyEE/Resources/CMSConfiguration.json",
+                           "watch_cms_config": "/WatchAppExtension/WatchCMS.json",
+                           "app_config": "/MyEE/Resources/MyeeConfig.json",
+                           "analytics_config": "/MyEE/Resources/AnalyticsContextData.json",
+                           "ab_test_config": "/MyEE/Resources/abtestconfig.json"}
+    if not project_dir:
+        return ".." + relative_local_path[config_name]
+    else:
+        return project_dir + relative_local_path[config_name]
 
-config_mapping = {"CMS_config": ("http://stubreena.co.uk/digital/cms/common/fallback-cms.json", "../MyEE/Resources/CMSConfiguration.json"),
-                  "watch_CMS_config": ("http://stubreena.co.uk/digital/cms/common/WatchCMS.json", "../WatchAppExtension/WatchCMS.json"),
-                  "app_config": ("https://s3-eu-west-1.amazonaws.com/ee-dtp-static-s3-test/prod/myeeapp/{}/v4app_config-ios.json".format(app_version_number), "../MyEE/Resources/MyeeConfig.json"),
-                  "analytics_config": ("http://stubreena.co.uk/digital/analytics/AnalyticsContextData.json", "../MyEE/Resources/MyeeConfig.json"),
-                  "ab_test_config": ("https://s3-eu-west-1.amazonaws.com/ee-dtp-static-s3-test/prod/myeeapp/abtestconfig/abtestconfig.json", "../MyEE/Resources/abtestconfig.json")}
+url_mapping = {"cms_config": {"development": "http://stubreena.co.uk/digital/cms/common/fallback-cms.json",
+                              "release": "http://stubreena.co.uk/digital/cms/common/fallback-cms.json"},
+               "watch_cms_config": {"devolopment": "http://stubreena.co.uk/digital/cms/common/WatchCMS.json",
+                                    "release": "http://stubreena.co.uk/digital/cms/common/WatchCMS.json"},
+               "app_config": {"development": "https://s3-eu-west-1.amazonaws.com/ee-dtp-static-s3-test/prod/myeeapp/{}/v4app_config-ios.json".format(app_version_number),
+                              "release": "https://s3-eu-west-1.amazonaws.com/ee-dtp-static-s3-test/prod/myeeapp/{}/v4app_config-ios.json".format(app_version_number)},
+               "analytics_config": {"development": "http://stubreena.co.uk/digital/analytics/AnalyticsContextData.json",
+                                    "release": "http://stubreena.co.uk/digital/analytics/AnalyticsContextData.json"},
+               "ab_test_config": {"development": "https://s3-eu-west-1.amazonaws.com/ee-dtp-static-s3-test/prod/myeeapp/abtestconfig/abtestconfig.json",
+                                  "release": "https://s3-eu-west-1.amazonaws.com/ee-dtp-static-s3-test/prod/myeeapp/abtestconfig/abtestconfig.json"}
+               }
 
 def get_latest(config_name: str):
-    url, path = config_mapping[config_name]
+    path = get_absolute_local_path(config_name)
+    url = url_mapping[config_name][current_branch]
     print("Downloading {} via url:\n{}".format(config_name, url))
     from requests import get
     response = get(url=url)
