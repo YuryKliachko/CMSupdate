e13ed63c97250f9865371e05498bc8ba147e9d0a
Yury Kliachko <uraklechko@gmail.com>
2018-11-11 08:48:46 +0300

    Try/except blocks for retrieving env vars (EAFP)

--- script.py
+++ script.py
@@ -55,7 +55,9 @@ async def get_latest(config_name):
     local_path = get_absolute_local_path(config_name)
     url = url_mapping[config_name]
     print("Downloading {} via url:\n{}".format(config_name, url))
+    # Run a blocking GET request in a separate executor
     request = _loop.run_in_executor(None, get, url)
+    # Take back a control to the calling coroutine until response received
     response = await request
     if response.status_code == 200:
         print("Remote for {} file successfully downloaded.".format(config_name))
@@ -74,29 +76,42 @@ async def get_latest(config_name):
 
 
 async def print_when_done(tasks):
-    """Top level coroutine, which execute tasks """
+    """Top level coroutine, which execute tasks. Receives list of tasks, returns"""
     for result in as_completed(tasks):
         print(await result)
 
 if __name__ == "__main__":
-    # Find PROJECT_DIR variable. If it does not exist, returns None
-    project_dir = environ.get("PROJECT_DIR", None)
-    # Retrieve  GCC_PREPROCESSOR_DEFINITIONS variable in order to define what scheme used
-    preprocessor_definition = environ.get("GCC_PREPROCESSOR_DEFINITIONS")
-    # If QA is set, then QA scheme is used. If it's absent, but TEMP_VAR123 (this name is not final) is present,
-    # it means that build is being created on a production scheme, but QA config files are needed. If PRODUCTION is set
-    # only,then a production scheme is used.
-    if "QA=1" in preprocessor_definition or environ.get("TEMP_VAR123", None) is not None:
-        url_mapping = config_scheme["qa"]["url_mapping"]
-    elif "PRODUCTION=1" in preprocessor_definition:
-        url_mapping = config_scheme["production"]["url_mapping"]
     print("\"app_version_number\" should be changed for each release manually.\n"
           "It means that each release branch which is in parallel should have its own related \"app_version_number\"")
+    # Find PROJECT_DIR variable. If it does not exist, returns None
+    try:
+        project_dir = environ.get("PROJECT_DIR")
+    except KeyError:
+        project_dir = None
+    # Retrieve  TEMP_VAR123 (this name is not final), which is going to be set if the app is being created with
+    # production schema, but QA config should be used.
+    try:
+        temp_var = environ.get("TEMP_VAR123", None)
+    except KeyError:
+        temp_var = None
+    # Retrieve  GCC_PREPROCESSOR_DEFINITIONS variable in order to define what scheme used
+    # If QA isnin GCC_PREPROCESSOR_DEFINITIONS or TEMP_VAR123 is present among global env variables,
+    # then QA URLs will be used in order to download config files. If PRODUCTION is set only,
+    # then a production scheme with the corresponding URLs will be used.
+    try:
+        preprocessor_definition = environ.get("GCC_PREPROCESSOR_DEFINITIONS")
+        if "QA=1" in preprocessor_definition or temp_var is not None:
+            url_mapping = config_scheme["qa"]["url_mapping"]
+        elif "PRODUCTION=1" in preprocessor_definition:
+            url_mapping = config_scheme["production"]["url_mapping"]
+    except KeyError:
+        print("Failed in retieving GCC_PREPROCESSOR_DEFINITIONS variable. It must be set in order to define a schema")
+        exit()
     # names of config files to be updated
     names = ("cms_config", "watch_cms_config", "app_config", "analytics_config", "ab_test_config")
-    # Create an event loop dor scheduling coroutines
+    # Create an event loop for scheduling coroutines
     loop = get_event_loop()
-    # Run coroutines in the loop
+    # Run a top level coroutine print_when_done.
     loop.run_until_complete(print_when_done([get_latest(config_name) for config_name in names]))
     # Close the loop when job is done
     loop.close()
